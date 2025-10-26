f8427ce0af60e7dbc29661640ada3757049f4f6b
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-10-27 06:56:53 +0800

    remove unused MIMEImage import

--- send_bulk_emails.py
+++ send_bulk_emails.py
@@ -3,7 +3,6 @@ import smtplib
 import pandas as pd
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 from email.mime.base import MIMEBase
 from email import encoders
 from dotenv import load_dotenv
