30589c3a61292486d11f2ab6c1a7ab89a3d3975d
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-10-27 06:56:24 +0800

    remove unused EmailMessage import

--- send_bulk_emails.py
+++ send_bulk_emails.py
@@ -1,7 +1,6 @@
 import os
 import smtplib
 import pandas as pd
-from email.message import EmailMessage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
